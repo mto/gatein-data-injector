a0febd5a0631f4f7b579eb8e6da9258ba6edba57
Minh Hoang TO <hoang281283@gmail.com>
2013-01-11 12:00:18 +0700

    Move PageDataInjector to page-relevant package

--- core/src/main/java/org/gatein/portal/injector/page/PageDataInjector.java
+++ core/src/main/java/org/gatein/portal/injector/page/PageDataInjector.java
@@ -20,7 +20,7 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.gatein.portal.injector.navigation;
+package org.gatein.portal.injector.page;
 
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.container.component.RequestLifeCycle;
